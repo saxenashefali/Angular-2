840fd1ad7a9b03ab884892a16f78c1311565bac3
saxenashefali <saxenashefali65@gmail.com>
2022-03-22 22:05:40 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { bufferToggle } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [
+    h3 {
+    color: blue ;
+    }
+  ]
 })
 export class AppComponent {
-  title = 'my-app';
+
 }
